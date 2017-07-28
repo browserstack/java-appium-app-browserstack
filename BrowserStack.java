079933e520a24e4a4799b838af5e3895d0833ee8
Harish Ved <ved.harish3@gmail.com>
2017-07-28 16:04:53 +0530

    Changes to run tests for app-automate

--- BrowserStack.java
+++ BrowserStack.java
@@ -1,34 +1,37 @@
-package bs;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import org.apache.commons.io.FileUtils;
+import java.net.MalformedURLException;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.MobileElement;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.Augmenter;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BrowserStack {
 
     public static void main(String args[]) throws MalformedURLException, InterruptedException {
-        DesiredCapabilities capability = DesiredCapabilities.firefox();
-        WebDriver driver = new RemoteWebDriver(
-            new URL("http://USERNAME:ACCESS_KEY@hub.browserstack.com/wd/hub"),
-            capability
-            );
-        driver.get("http://www.google.com");
-        System.out.println("Page title is: " + driver.getTitle());
-        WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("BrowserStack");
-        element.submit();
-        System.out.println("And the Title is: " + driver.getTitle());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("realMobile", true);
+        capabilities.setCapability("device", "Samsung Galaxy S6");
+        capabilities.setCapability("app", "bs://3fc0a1f5a158e935ad806b97288f4b24e11ebcc4");
+
+        AndroidDriver driver = new AndroidDriver(new URL("http://BROWSERSTACK_USERNAME:BROWSERSTACK_ACCESS_KEY@hub.browserstack.com/wd/hub"), capabilities);
+
+        WebElement searchElement = new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(By.id("Search Wikipedia")));
+        searchElement.click();
+        WebElement insertTextElement = new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(By.id("org.wikipedia.alpha:id/search_src_text")));
+        insertTextElement.sendKeys("BrowserStack");
+        Thread.sleep(5000);
+
+        List<WebElement> allProductsName = driver.findElements(By.className("android.widget.TextView"));
+        assertTrue(allProductsName.size() > 0);
+
         driver.quit();
     }
 }
--- LocalSample.java
+++ LocalSample.java
@@ -1,20 +1,73 @@
+import com.browserstack.local.Local;
+
+import java.net.URL;
+import java.net.MalformedURLException;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.MobileElement;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class LocalSample {
+    private static Local localInstance;
+
+    public static void setupLocal() throws Exception {
+      localInstance = new Local();
+      Map<String, String> options = new HashMap<String, String>();
+      options.put("key", accessKey);
+      localInstance.start(options);
+    }
+
+    public static void tearDownLocal() throws Exception {
+      localInstance.stop();
+    }
 
     public static void main(String[] args) throws Exception {
+        setupLocal();
 
-        WebDriver driver = new FirefoxDriver();
-        driver.get("http://www.google.com/");
-        System.out.println("Page title is: " + driver.getTitle());
-        WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("BrowserStack");
-        element.submit();
-        System.out.println("And the Title is: " + driver.getTitle());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("browserstack.local", true);
+        capabilities.setCapability("realMobile", true);
+        capabilities.setCapability("device", "Samsung Galaxy S6");
+        capabilities.setCapability("app", "bs://3fc0a1f5a158e935ad806b97288f4b24e11ebcc4");
+
+        AndroidDriver driver = new AndroidDriver(new URL("http://BROWSERSTACK_USERNAME:BROWSERSTACK_ACCESS_KEY@hub.browserstack.com/wd/hub"), capabilities);
+
+        WebElement searchElement = new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(By.id("com.example.android.basicnetworking:id/test_action")));
+        searchElement.click();
+        WebElement insertTextElement = new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(By.className("android.widget.TextView")));
+
+        WebElement testElement = null;
+        List<WebElement> allTextViewElements = driver.findElements(By.className("android.widget.TextView"));
+        Thread.sleep(10);
+        for(WebElement textElement : allTextViewElements) {
+          System.out.println(textElement.getText());
+          if(textElement.getText().contains("The active connection is")) {
+            testElement = textElement;
+          }
+        }
+
+        if(testElement == null) {
+          File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+          FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir") + "screenshot.png"));
+          System.out.println("Screenshot stored at " + System.getProperty("user.dir") + "screenshot.png");
+          throw new Error("Cannot find the needed TextView element from app");
+        }
+        String matchedString = testElement.getText();
+        System.out.println(matchedString);
+        assertTrue(matchedString.contains("The active connection is wifi"));
+        assertTrue(matchedString.contains("Up and running"));
+
         driver.quit();
 
+        tearDownLocal();
     }
 }
