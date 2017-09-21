f2b96e2545c3c79abf8b9bd933fd5c24c7336b62
Tejas Shah <tejas.s@browserstack.com>
2017-09-22 02:21:38 +0530

    use Android Driver Element instead of Selenium

--- android/BrowserStackAndroid.java
+++ android/BrowserStackAndroid.java
@@ -1,16 +1,17 @@
 import java.net.URL;
 import java.util.List;
 import java.net.MalformedURLException;
-import io.appium.java_client.android.AndroidDriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BrowserStackAndroid {
-	
+
     public static String accessKey = "BROWSERSTACK_USERNAME";
     public static String userName = "BROWSERSTACK_ACCESS_KEY";
 
@@ -21,17 +22,17 @@ public class BrowserStackAndroid {
         capabilities.setCapability("device", "Samsung Galaxy S7");
         capabilities.setCapability("app", "bs://<hashed app-id>");
 
-        AndroidDriver driver = new AndroidDriver(new URL("https://"+userName+":"+accessKey+"@hub.browserstack.com/wd/hub"), capabilities);
+        AndroidDriver driver = new AndroidDriver<AndroidElement>(new URL("https://"+userName+":"+accessKey+"@hub.browserstack.com/wd/hub"), capabilities);
 
-        WebElement searchElement = new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(By.id("Search Wikipedia")));
+        AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia")));
         searchElement.click();
-        WebElement insertTextElement = new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(By.id("org.wikipedia.alpha:id/search_src_text")));
+        AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
         insertTextElement.sendKeys("BrowserStack");
         Thread.sleep(5000);
 
-        List<WebElement> allProductsName = driver.findElements(By.className("android.widget.TextView"));
+        List<AndroidElement> allProductsName = driver.findElementsByClassName("android.widget.TextView");
         assert(allProductsName.size() > 0);
 
         driver.quit();
