1924c1642a89fcd88345f70a3d65472fda2238d0
srishti11010 <srishti@browserstack.com>
2018-05-02 16:53:12 +0530

    Changes for new sample app

--- ios/BrowserStackIOS.java
+++ ios/BrowserStackIOS.java
@@ -16,36 +16,30 @@ public class BrowserStackIOS {
     public static String userName = "BROWSERSTACK_ACCESS_KEY";
 
     public static void main(String args[]) throws MalformedURLException, InterruptedException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        DesiredCapabilities caps = new DesiredCapabilities();
 
-        capabilities.setCapability("device", "iPhone 7");
-        capabilities.setCapability("app", "bs://<hashed app-id>");
+        caps.setCapability("device", "iPhone 7 Plus");
+        caps.setCapability("app", "bs://<hashed app-id>");
 
-        IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(new URL("http://"+userName+":"+accessKey+"@hub.browserstack.com/wd/hub"), capabilities);
+        IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(new URL("http://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), caps);
 
-        IOSElement loginButton = (IOSElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Log In")));
-        loginButton.click();
-        IOSElement emailTextField = (IOSElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Email address")));
-        emailTextField.sendKeys("hello@browserstack.com");
+        IOSElement textButton = (IOSElement) new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Button")));
+        textButton.click();
+        IOSElement textInput = (IOSElement) new WebDriverWait(driver, 30).until(
+            ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Input")));
+        textInput.sendKeys("hello@browserstack.com");
 
-        driver.findElementByAccessibilityId("Next").click();      
         Thread.sleep(5000);
 
-        List<IOSElement> textElements = driver.findElementsByXPath("//XCUIElementTypeStaticText");
-        assert(textElements.size() > 0);
-        String matchedString = "";
-        for(IOSElement textElement : textElements) {
-          String textContent = textElement.getText();
-          if(textContent != null && textContent.contains("not registered")) {
-            matchedString = textContent;
-          }
-        }
-
-        System.out.println(matchedString);
-        assert(matchedString.contains("not registered on WordPress.com"));
-
+        IOSElement textOutput = (IOSElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Output")));
+    
+        if(textOutput != null && textOutput.getText().equals("hello@browserstack.com"))
+            assert(true);
+        else
+            assert(false);  
+    
         driver.quit();
     }
 }
