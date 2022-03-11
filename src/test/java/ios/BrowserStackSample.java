15c4e051db867946882d617de3d9678fd3e430a0
Abhishek Jha <21086203+innovater21@users.noreply.github.com>
2022-03-11 15:00:32 +0530

    added w3c capabilities

--- src/test/java/ios/BrowserStackSample.java
+++ src/test/java/ios/BrowserStackSample.java
@@ -16,50 +16,50 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class BrowserStackSample {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-   	DesiredCapabilities caps = new DesiredCapabilities();
-    	
-    	// Set your access credentials
-    	caps.setCapability("browserstack.user", "YOUR_USERNAME");
-    	caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
+        
+        DesiredCapabilities caps = new DesiredCapabilities();
+            
+        // Set your access credentials
+        caps.setCapability("browserstack.user", "YOUR_USERNAME");
+		caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
     	
     	// Set URL of the application under test
     	caps.setCapability("app", "bs://<app-id>");
-    	
-    	// Specify device and os_version for testing
-    	caps.setCapability("device", "iPhone 11 Pro");
-    	caps.setCapability("os_version", "13");
         
-    	// Set other BrowserStack capabilities
-    	caps.setCapability("project", "First Java Project");
-    	caps.setCapability("build", "browserstack-build-1");
-    	caps.setCapability("name", "first_test");
-    	
-    	
-    	// Initialise the remote Webdriver using BrowserStack remote URL
-    	// and desired capabilities defined above
+        // Specify device and os_version for testing
+        caps.setCapability("deviceName", "iPhone 11 Pro");
+        caps.setCapability("platformName", "ios");
+    	caps.setCapability("platformVersion", "13");
+        
+        // Set other BrowserStack capabilities
+        caps.setCapability("project", "First Java Project");
+        caps.setCapability("build", "browserstack-build-1");
+        caps.setCapability("name", "first_test");
+        
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
         RemoteWebDriver driver = new RemoteWebDriver(
-        		new URL("http://hub-cloud.browserstack.com/wd/hub"), caps);
+                new URL("http://hub-cloud.browserstack.com/wd/hub"), caps);
         
-
         // Test case for the BrowserStack sample iOS app. 
         // If you have uploaded your app, update the test case here. 
         WebElement textButton = (WebElement) new WebDriverWait(driver, 30).until(
             ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Button")));
         textButton.click();
+
         WebElement textInput = (WebElement) new WebDriverWait(driver, 30).until(
             ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Input")));
         textInput.sendKeys("hello@browserstack.com");
         Thread.sleep(5000);
+
         WebElement textOutput = (WebElement) new WebDriverWait(driver, 30).until(
                 ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Output")));
         if(textOutput != null && textOutput.getText().equals("hello@browserstack.com"))
             assert(true);
         else
             assert(false);  
-    
+
         // Invoke driver.quit() after the test is done to indicate that the test is completed.
         driver.quit();
-
-	}
-
+    }
 }
