680d0701ae9ce69129fccacb875e358a82d7a544
Abhishek Jha <21086203+innovater21@users.noreply.github.com>
2022-03-11 16:19:59 +0530

    added support for w3c and upgrade

--- src/test/java/ios/BrowserStackSampleLocal.java
+++ src/test/java/ios/BrowserStackSampleLocal.java
@@ -1,99 +1,124 @@
 package ios;
 
 import com.browserstack.local.Local;
-import java.net.URL; import java.io.File; import java.util.*;
+import io.appium.java_client.AppiumBy;
+import java.io.File;
+import java.net.URL;
+import java.time.Duration;
+import java.util.*;
 import org.apache.commons.io.FileUtils;
-import io.appium.java_client.ios.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.*;import org.openqa.selenium.remote.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.*;
 
 public class BrowserStackSampleLocal {
-	
-    private static Local localInstance;
-    public static String userName = "YOUR_USERNAME";
-    public static String accessKey = "YOUR_ACCESS_KEY";
-  
 
-    public static void setupLocal() throws Exception {
-      localInstance = new Local();
-      Map<String, String> options = new HashMap<String, String>();
-      options.put("key", accessKey);
-      localInstance.start(options);
-    }
+  private static Local localInstance;
+  public static String userName = "YOUR_USERNAME";
+  public static String accessKey = "YOUR_ACCESS_KEY";
 
-    public static void tearDownLocal() throws Exception {
-      localInstance.stop();
-    }
+  public static void setupLocal() throws Exception {
+    localInstance = new Local();
+    Map<String, String> options = new HashMap<String, String>();
+    options.put("key", accessKey);
+    options.put("local", "true");
+    localInstance.start(options);
+  }
 
+  public static void tearDownLocal() throws Exception {
+    localInstance.stop();
+  }
 
-	public static void main(String[] args) throws Exception {
-        // Start the BrowserStack Local binary
-        setupLocal();
+  public static void main(String[] args) throws Exception {
+    // Start the BrowserStack Local binary
+    setupLocal();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+    DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        // Set your access credentials
-    	  capabilities.setCapability("browserstack.user", userName);
-        capabilities.setCapability("browserstack.key", accessKey);
-        
-        // Set URL of the application under test
-       capabilities.setCapability("app", "bs://<app-id>");
-       
-       // Specify device and os_version for testing
-       capabilities.setCapability("device", "iPhone 11 Pro");
-       capabilities.setCapability("os_version", "13");
+    // Set your access credentials
+    capabilities.setCapability("browserstack.user", userName);
+    capabilities.setCapability("browserstack.key", accessKey);
 
-      // Set the browserstack.local capability to true
-      capabilities.setCapability("browserstack.local", true);
+    // Set URL of the application under test
+    capabilities.setCapability("app", "<app-id>");
 
-      // Set other BrowserStack capabilities
-    	capabilities.setCapability("project", "First Java Project");
-    	capabilities.setCapability("build", "browserstack-build-1");
-    	capabilities.setCapability("name", "local_test");
-        
-   	  // Initialise the remote Webdriver using BrowserStack remote URL
-      // and desired capabilities defined above
-      RemoteWebDriver driver = new RemoteWebDriver(
-        new URL("http://hub.browserstack.com/wd/hub"), capabilities);
+    // Specify device and os_version for testing
+    capabilities.setCapability("deviceName", "iPhone 11 Pro");
+    capabilities.setCapability("platformName", "ios");
+    capabilities.setCapability("platformVersion", "13");
 
-        // Test case for the BrowserStack sample iOS Local app. 
-        // If you have uploaded your app, update the test case here.   
-        WebElement testButton = (WebElement) new WebDriverWait(driver, 30).until(
-          ExpectedConditions.elementToBeClickable(By.xpath("//*[@content-desc='TestBrowserStackLocal']")));
-        testButton.click();
-        
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        wait.until(new ExpectedCondition<Boolean>() {
-          @Override
-          public Boolean apply(WebDriver d) {
-            String result = d.findElement(By.xpath("//*[@content-desc='ResultBrowserStackLocal']")).getAttribute("value");
-            return result != null && result.length() > 0;
-          }
-        });
-        WebElement resultElement = (WebElement) driver.findElement(By.xpath("//*[@content-desc='ResultBrowserStackLocal']"));
+    // Set the browserstack.local capability to true
+    capabilities.setCapability("browserstack.local", true);
 
-        String resultString = resultElement.getText().toLowerCase();
-        System.out.println(resultString);
-        if(resultString.contains("not working")) {
-          File scrFile = (File) ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-          FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir") + "/screenshot.png"));
-          System.out.println("Screenshot stored at " + System.getProperty("user.dir") + "/screenshot.png");
-          throw new Error("Unexpected BrowserStackLocal test result");
+    // Set other BrowserStack capabilities
+    capabilities.setCapability("project", "First Java Project");
+    capabilities.setCapability("build", "browserstack-build-1");
+    capabilities.setCapability("name", "local_test");
+
+    // Initialise the remote Webdriver using BrowserStack remote URL
+    // and desired capabilities defined above
+    RemoteWebDriver driver = new RemoteWebDriver(
+      new URL("http://hub.browserstack.com/wd/hub"),
+      capabilities
+    );
+
+    // Test case for the BrowserStack sample iOS Local app.
+    // If you have uploaded your app, update the test case here.
+    WebElement testButton = (WebElement) new WebDriverWait(
+      driver,
+      Duration.ofSeconds(30)
+    )
+    .until(
+        ExpectedConditions.elementToBeClickable(
+          AppiumBy.accessibilityId("TestBrowserStackLocal")
+        )
+      );
+    testButton.click();
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    wait.until(
+      new ExpectedCondition<Boolean>() {
+        @Override
+        public Boolean apply(WebDriver d) {
+          String result = d
+            .findElement(AppiumBy.accessibilityId("ResultBrowserStackLocal"))
+            .getAttribute("value");
+          return result != null && result.length() > 0;
         }
+      }
+    );
+    WebElement resultElement = (WebElement) driver.findElement(
+      AppiumBy.accessibilityId("ResultBrowserStackLocal")
+    );
 
-        String expectedString = "Up and running";
-        assert(resultString.contains(expectedString.toLowerCase()));
+    String resultString = resultElement.getText().toLowerCase();
+    System.out.println(resultString);
+    if (resultString.contains("not working")) {
+      File scrFile = (File) ((TakesScreenshot) driver).getScreenshotAs(
+          OutputType.FILE
+        );
+      FileUtils.copyFile(
+        scrFile,
+        new File(System.getProperty("user.dir") + "/screenshot.png")
+      );
+      System.out.println(
+        "Screenshot stored at " +
+        System.getProperty("user.dir") +
+        "/screenshot.png"
+      );
+      throw new Error("Unexpected BrowserStackLocal test result");
+    }
 
-        // Invoke driver.quit() after the test is done to indicate that the test is completed.
-        driver.quit();
-        
-        // Stop the BrowserStack Local binary
-        tearDownLocal();
-		
-	}
+    String expectedString = "Up and running";
+    assert (resultString.contains(expectedString.toLowerCase()));
 
+    // Invoke driver.quit() after the test is done to indicate that the test is completed.
+    driver.quit();
+
+    // Stop the BrowserStack Local binary
+    tearDownLocal();
+  }
 }
