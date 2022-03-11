500451967e7e1b4cb2dc4d5edc4c20518211e821
Abhishek Jha <21086203+innovater21@users.noreply.github.com>
2022-03-11 16:22:54 +0530

    removed deprecated syntax

--- src/test/java/android/BrowserStackSample.java
+++ src/test/java/android/BrowserStackSample.java
@@ -1,65 +1,71 @@
 package android;
 
-import java.net.URL;
-import java.util.List;
-import java.util.function.Function;
-import java.net.MalformedURLException;
-
 import io.appium.java_client.AppiumBy;
-import org.openqa.selenium.WebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BrowserStackSample {
 
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-		
-    	DesiredCapabilities caps = new DesiredCapabilities();
-    	
-    	// Set your access credentials
-    	caps.setCapability("browserstack.user", "YOUR_USERNAME");
-		caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
-    	
-    	// Set URL of the application under test
-    	caps.setCapability("app", "bs://<app-id>");
-    	
-    	// Specify deviceName and platformName for testing
-    	caps.setCapability("deviceName", "Google Pixel 3");
-		caps.setCapability("platformName", "android");
-    	caps.setCapability("platformVersion", "9.0");
-        
-    	// Set other BrowserStack capabilities
-    	caps.setCapability("project", "First Java Project");
-    	caps.setCapability("build", "browserstack-build-1");
-    	caps.setCapability("name", "first_test");
-       
-    	// Initialise the remote Webdriver using BrowserStack remote URL
-    	// and desired capabilities defined above
-    	RemoteWebDriver driver = new RemoteWebDriver(new URL("http://hub.browserstack.com/wd/hub"), caps);
+  public static void main(String[] args)
+    throws MalformedURLException, InterruptedException {
+    DesiredCapabilities caps = new DesiredCapabilities();
 
-        // Test case for the BrowserStack sample Android app. 
-        // If you have uploaded your app, update the test case here. 
-        WebElement searchElement = (WebElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(
-            		AppiumBy.accessibilityId("Search Wikipedia")));
-        searchElement.click();
+    // Set your access credentials
+    caps.setCapability("browserstack.user", "YOUR_USERNAME");
+    caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
 
-		WebElement insertTextElement = (WebElement) new WebDriverWait(driver, 30).until(
-             ExpectedConditions.elementToBeClickable(
-            		 AppiumBy.id("org.wikipedia.alpha:id/search_src_text")));
-        insertTextElement.sendKeys("BrowserStack");
-        Thread.sleep(5000);
+    // Set URL of the application under test
+    caps.setCapability("app", "bs://<app-id>");
 
-        List<WebElement> allProductsName = driver.findElements(AppiumBy.className("android.widget.TextView"));
-        assert(allProductsName.size() > 0);
-        
-        // Invoke driver.quit() after the test is done to indicate that the test is completed.
-        driver.quit();
-		
-	}
+    // Specify deviceName and platformName for testing
+    caps.setCapability("deviceName", "Google Pixel 3");
+    caps.setCapability("platformName", "android");
+    caps.setCapability("platformVersion", "9.0");
 
+    // Set other BrowserStack capabilities
+    caps.setCapability("project", "First Java Project");
+    caps.setCapability("build", "browserstack-build-1");
+    caps.setCapability("name", "first_test");
+
+    // Initialise the remote Webdriver using BrowserStack remote URL
+    // and desired capabilities defined above
+    RemoteWebDriver driver = new RemoteWebDriver(
+      new URL("http://hub.browserstack.com/wd/hub"),
+      caps
+    );
+
+    // Test case for the BrowserStack sample Android app.
+    // If you have uploaded your app, update the test case here.
+    WebElement searchElement = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30))
+    .until(
+        ExpectedConditions.elementToBeClickable(
+          AppiumBy.accessibilityId("Search Wikipedia")
+        )
+      );
+    searchElement.click();
+
+    WebElement insertTextElement = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30))
+    .until(
+        ExpectedConditions.elementToBeClickable(
+          AppiumBy.id("org.wikipedia.alpha:id/search_src_text")
+        )
+      );
+    insertTextElement.sendKeys("BrowserStack");
+    Thread.sleep(5000);
+
+    List<WebElement> allProductsName = driver.findElements(
+      AppiumBy.className("android.widget.TextView")
+    );
+    assert (allProductsName.size() > 0);
+
+    // Invoke driver.quit() after the test is done to indicate that the test is completed.
+    driver.quit();
+  }
 }
--- src/test/java/ios/BrowserStackSample.java
+++ src/test/java/ios/BrowserStackSample.java
@@ -1,65 +1,77 @@
 package ios;
 
-import java.net.URL;
-import java.util.List;
-import java.net.MalformedURLException;
-
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import io.appium.java_client.AppiumBy;
-import org.openqa.selenium.WebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowserStackSample {
 
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-        
-        DesiredCapabilities caps = new DesiredCapabilities();
-            
-        // Set your access credentials
-        caps.setCapability("browserstack.user", "YOUR_USERNAME");
-		caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
-    	
-    	// Set URL of the application under test
-    	caps.setCapability("app", "bs://<app-id>");
-        
-        // Specify device and os_version for testing
-        caps.setCapability("deviceName", "iPhone 11 Pro");
-        caps.setCapability("platformName", "ios");
-    	caps.setCapability("platformVersion", "13");
-        
-        // Set other BrowserStack capabilities
-        caps.setCapability("project", "First Java Project");
-        caps.setCapability("build", "browserstack-build-1");
-        caps.setCapability("name", "first_test");
-        
-        // Initialise the remote Webdriver using BrowserStack remote URL
-        // and desired capabilities defined above
-        RemoteWebDriver driver = new RemoteWebDriver(
-                new URL("http://hub-cloud.browserstack.com/wd/hub"), caps);
-        
-        // Test case for the BrowserStack sample iOS app. 
-        // If you have uploaded your app, update the test case here. 
-        WebElement textButton = (WebElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Button")));
-        textButton.click();
+  public static void main(String[] args)
+    throws MalformedURLException, InterruptedException {
+    DesiredCapabilities caps = new DesiredCapabilities();
 
-        WebElement textInput = (WebElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Input")));
-        textInput.sendKeys("hello@browserstack.com");
-        Thread.sleep(5000);
+    // Set your access credentials
+    caps.setCapability("browserstack.user", "YOUR_USERNAME");
+    caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
 
-        WebElement textOutput = (WebElement) new WebDriverWait(driver, 30).until(
-                ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Text Output")));
-        if(textOutput != null && textOutput.getText().equals("hello@browserstack.com"))
-            assert(true);
-        else
-            assert(false);  
+    // Set URL of the application under test
+    caps.setCapability("app", "bs://<app-id>");
 
-        // Invoke driver.quit() after the test is done to indicate that the test is completed.
-        driver.quit();
-    }
+    // Specify device and os_version for testing
+    caps.setCapability("deviceName", "iPhone 11 Pro");
+    caps.setCapability("platformName", "ios");
+    caps.setCapability("platformVersion", "13");
+
+    // Set other BrowserStack capabilities
+    caps.setCapability("project", "First Java Project");
+    caps.setCapability("build", "browserstack-build-1");
+    caps.setCapability("name", "first_test");
+
+    // Initialise the remote Webdriver using BrowserStack remote URL
+    // and desired capabilities defined above
+    RemoteWebDriver driver = new RemoteWebDriver(
+      new URL("http://hub-cloud.browserstack.com/wd/hub"),
+      caps
+    );
+
+    // Test case for the BrowserStack sample iOS app.
+    // If you have uploaded your app, update the test case here.
+    WebElement textButton = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30))
+    .until(
+        ExpectedConditions.elementToBeClickable(
+          AppiumBy.accessibilityId("Text Button")
+        )
+      );
+    textButton.click();
+
+    WebElement textInput = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30))
+    .until(
+        ExpectedConditions.elementToBeClickable(
+          AppiumBy.accessibilityId("Text Input")
+        )
+      );
+    textInput.sendKeys("hello@browserstack.com");
+    Thread.sleep(5000);
+
+    WebElement textOutput = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30))
+    .until(
+        ExpectedConditions.elementToBeClickable(
+          AppiumBy.accessibilityId("Text Output")
+        )
+      );
+    if (
+      textOutput != null &&
+      textOutput.getText().equals("hello@browserstack.com")
+    ) assert (true); else assert (false);
+
+    // Invoke driver.quit() after the test is done to indicate that the test is completed.
+    driver.quit();
+  }
 }
